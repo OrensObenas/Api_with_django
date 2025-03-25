bfb6469ec2a52bb0b3d1a3664420a44d94a5a629
OrensObenas <gilbertobenas@gmail.com>
2025-03-25 18:10:01 +0000

    Mise en place des operations crud dans LittleLemon avec des vues génériques en utilisant moins de code

--- LittleLemon/LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemon/LittleLemonAPI/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .models import MenuItem
+from .serializers import MenuItemSerializer
 
 # Create your views here.
+class MenuItemsView(generics.ListCreateAPIView):
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
+    
+class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
+    queryset = MenuItem.objects.all() # Pour afficher les elements
+    serializer_class = MenuItemSerializer # Pour Effectuer les operations CRUD
