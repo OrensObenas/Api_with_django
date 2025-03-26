ef0d46fd9107933d858533b20298f96abaedc70e
OrensObenas <gilbertobenas@gmail.com>
2025-03-26 09:21:04 +0000

    Créer une relation de dépendance entre serializers et les afficher grace au apiview

--- LittleLemon/LittleLemon/LittleLemonAPI/views.py
+++ LittleLemon/LittleLemon/LittleLemonAPI/views.py
@@ -1,9 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from rest_framework import generics
 from .models import MenuItem
 from .serializers import MenuItemSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
 # Create your views here.
+@api_view()
+def menu_items(request):
+    items = MenuItem.objects.select_related('category').all()
+    serializer_item = MenuItemSerializer(items, many=True)
+    return Response(serializer_item.data)
+
+@api_view()
+def single_item(request, id):
+    item = get_object_or_404(MenuItem, pk=id)
+    serializer_item = MenuItemSerializer(item)
+    return Response(serializer_item.data)
+
+
 class MenuItemsView(generics.ListCreateAPIView):
     queryset = MenuItem.objects.all()
     serializer_class = MenuItemSerializer
